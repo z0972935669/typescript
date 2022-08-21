378ab77ea5678a561413b1db3db7f436bd21037d
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-21 15:44:39 +0800

    update file

---
+++ code/chapter01/part2/dist/app.js
@@ -0,0 +1,9 @@
+// import {hi} from 'm.js';
+console.log('hello');
+let a = 10;
+// console.log(hi);
+console.log(a);
+// export const hi = '你好';
+let b = 20;
+let c = 'hello';
+console.log(b, c);
--- code/chapter01/part2/src/app.ts
+++ code/chapter01/part2/src/app.ts
@@ -1,1 +1,5 @@
+// import {hi} from 'm.js';
 console.log('hello');
+let a = 10;
+// console.log(hi);
+console.log(a);
